c6c4edc365c937245c8809eebeda7441bd7be60c
Bren <brennenbates2859@gmail.com>
2019-11-20 14:10:09 -0700

    Copied over original burger code
    copied over original burger code

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name,devoured) 
+VALUES('Veggie Burger', false);
+
+INSERT INTO burgers(burger_name,devoured) 
+VALUES('Big Benny', false);
+
+INSERT INTO burgers(burger_name,devoured) 
+VALUES('PB and J Burger', false);
+
+SELECT * FROM burgers;
+
+
